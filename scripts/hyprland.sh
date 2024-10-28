40644ac0ad1f20582a9fabb0496f120adb502b19
hoangloi07 <hoangloi31052007@gmail.com>
2024-10-29 00:08:20 +0700

    push

---
+++ scripts/hyprland.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo dnf copr enable solopasha/hyprland
+sudo dnf copr enable heus-sueh/packages
+sudo dnf config-manager --save --setopt=copr:copr.fedorainfracloud.org:heus-sueh:packages.priority=200
